479182f0e14f6b895b87fdc7a4d6df810ff69113
thel <thel@am-tech.co.jp>
2024-07-05 09:35:50 +0900

    add spring project

---
+++ runnerz/src/main/java/dev/choi/runnerz/RunnerzApplication.java
@@ -0,0 +1,20 @@
+package dev.choi.runnerz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerzApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerzApplication.class, args);
+
+		WelcomeMessage welcomeMessage = new WelcomeMessage();
+		System.out.println(welcomeMessage.getWelcomeMessage());
+	}
+
+}
---
+++ runnerz/src/main/java/dev/choi/runnerz/WelcomeMessage.java
@@ -0,0 +1,8 @@
+package dev.choi.runnerz;
+
+public class WelcomeMessage {
+
+    public String getWelcomeMessage() {
+        return "Hello World!";
+    }
+}
